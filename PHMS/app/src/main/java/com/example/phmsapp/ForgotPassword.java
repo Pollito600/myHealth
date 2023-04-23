efe2969361a7cc456eac7fb666ff344b1cc54c02
Joshua Catalan <joshcatalan14@gmail.com>
2023-04-23 15:50:30 -0500

    Forgot Password

--- PHMS/app/src/main/java/com/example/phmsapp/ForgotPassword.java
+++ PHMS/app/src/main/java/com/example/phmsapp/ForgotPassword.java
@@ -1,14 +1,63 @@
 package com.example.phmsapp;
 
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonResetPassword).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetPassword();
+            }
+        });
+    }
+
+    private void resetPassword() {
+        String email = editTextEmail.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ForgotPassword.this, "Check your email to reset your password!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ForgotPassword.this, "Failed to send reset password email! Please check email address.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
