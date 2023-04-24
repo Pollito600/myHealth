344a6774692be1bf36bd78a162f0c972dd3d76ca
Big-H65 <129345038+Big-H65@users.noreply.github.com>
2023-04-23 19:15:23 -0500

    FINALLY NOTES WORKS
    i want to die but notes works also dashboard works too thank you goodnight

--- PHMS/app/src/main/java/com/example/phmsapp/NotesActivity.java
+++ PHMS/app/src/main/java/com/example/phmsapp/NotesActivity.java
@@ -1,84 +1,194 @@
 package com.example.phmsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.ListView;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import java.util.ArrayList;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
-public class NotesActivity extends AppCompatActivity {
+import java.util.HashSet;
+import java.util.Set;
 
-    private ListView mNotesListView;
-    private Button mNewNoteButton;
-    private Button mOpenNoteButton;
-    private Button mModifyNoteButton;
-    private ArrayList<Note> notes;
-    private NoteAdapter noteAdapter;
+import android.widget.ArrayAdapter;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+public class NotesActivity extends AppCompatActivity {
+    private Button newNoteBtn;
+    private ListView notesListView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> notesList;
+    private SharedPreferences sharedPreferences;
+    private static final String NOTES_PREFS = "notes_prefs";
+    private static final String NOTES_LIST_KEY = "notes_list_key";
+    private void saveNotes(ArrayList<String> notesList) {
+        SharedPreferences sharedPreferences = getSharedPreferences("notes", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // convert the ArrayList to a Set to save it in SharedPreferences
+        Set<String> notesSet = new HashSet<String>(notesList);
+
+        editor.putStringSet("notes", notesSet);
+        editor.apply();
+    }
+    private void deleteNote(int position) {
+        // remove the note from the notesList
+        notesList.remove(position);
+        adapter.notifyDataSetChanged();
+
+        // remove the note from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("notes_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NOTES_LIST_KEY, new Gson().toJson(notesList));
+        editor.apply();
+        Log.d("NotesActivity", "Deleted note at position " + position);
+    }
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /*super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
-        mNotesListView = findViewById(R.id.notes_listview);
-        mNewNoteButton = findViewById(R.id.new_note_button);
-        mOpenNoteButton = findViewById(R.id.open_note_button);
-        mModifyNoteButton = findViewById(R.id.modify_note_button);
-        mNewNoteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle the "New Note" button click
-                // You can launch a new activity to create a new note, for example
-            }
-        });
-
-        mOpenNoteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle the "Open Note" button click
-                // You can launch a new activity to open an existing note, for example
-            }
-        });
-
-        mModifyNoteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Handle the "Modify Note" button click
-                // You can launch a new activity to modify an existing note, for example
-            }
-        });*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-        Log.d("NotesActivity", "onCreate() called");
-        mNotesListView = findViewById(R.id.notes_listview);
-        mNewNoteButton = findViewById(R.id.new_note_button);
-        notes = new ArrayList<>();
-        noteAdapter = new NoteAdapter(this, R.layout.activity_notes, notes);
-        mNotesListView.setAdapter(noteAdapter);
 
-        mNewNoteButton.setOnClickListener(new View.OnClickListener() {
+        newNoteBtn = findViewById(R.id.new_note_button);
+        notesListView = findViewById(R.id.notes_list_view);
+        notesList = new ArrayList<String>();
+
+        // Initialize shared preferences
+        sharedPreferences = getSharedPreferences(NOTES_PREFS, MODE_PRIVATE);
+
+        // Load notes from shared preferences
+        String notesJson = sharedPreferences.getString(NOTES_LIST_KEY, "");
+        if (!notesJson.isEmpty()) {
+            notesList = new Gson().fromJson(notesJson, new TypeToken<ArrayList<String>>(){}.getType());
+        }
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notesList);
+        notesListView.setAdapter(adapter);
+
+        newNoteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // Handle the "New Note" button click
-                notes.add(new Note("New Note", ""));
-                noteAdapter.notifyDataSetChanged();
-                Toast.makeText(NotesActivity.this, "New Note added", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                // create a new note dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(NotesActivity.this);
+                builder.setTitle("New Note");
+
+                // Set up the input
+                final EditText inputTitle = new EditText(NotesActivity.this);
+                final EditText inputBody = new EditText(NotesActivity.this);
+
+                // Set up the input fields
+                inputTitle.setHint("Note title");
+                inputBody.setHint("Note body");
+
+                LinearLayout layout = new LinearLayout(NotesActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                layout.addView(inputTitle);
+                layout.addView(inputBody);
+                builder.setView(layout);
+
+                // Set up the buttons
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String title = inputTitle.getText().toString();
+                        String body = inputBody.getText().toString();
+                        String note = title + "\n" + body;
+
+                        notesList.add(note);
+                        adapter.notifyDataSetChanged();
+
+                        // Save notes to shared preferences
+                        String notesJson = new Gson().toJson(notesList);
+                        sharedPreferences.edit().putString(NOTES_LIST_KEY, notesJson).apply();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Show the dialog
+                builder.show();
+            }
+        });
+
+        notesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedNote = notesList.get(position);
+                String[] noteParts = selectedNote.split("\n");
+
+                // create a new note dialog with the selected note details
+                AlertDialog.Builder builder = new AlertDialog.Builder(NotesActivity.this);
+                builder.setTitle("Edit Note");
+
+                // Set up the input
+                final EditText inputTitle = new EditText(NotesActivity.this);
+                final EditText inputBody = new EditText(NotesActivity.this);
+
+                // Set up the input fields
+                inputTitle.setHint("Note title");
+                inputTitle.setText(noteParts[0]);
+                inputBody.setHint("Note body");
+                inputBody.setText(noteParts[1]);
+
+                LinearLayout layout = new LinearLayout(NotesActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                layout.addView(inputTitle);
+                layout.addView(inputBody);
+                builder.setView(layout);
+
+                // Set up the buttons
+                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String title = inputTitle.getText().toString();
+                        String body = inputBody.getText().toString();
+                        String note = title + "\n" + body;
+
+                        // update the selected note in the notesList
+                        notesList.set(position, note);
+                        adapter.notifyDataSetChanged();
+
+                        // save the updated notes list in SharedPreferences
+                        saveNotes(notesList);
+                    }
+                });
+                builder.setNeutralButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        /*notesList.remove(position);
+                        adapter.notifyDataSetChanged();
+                        saveNotes(notesList);*/
+                        deleteNote(position);
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                // Show the dialog
+                builder.show();
             }
         });
     }
-
 }
--- PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
+++ PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
@@ -1,7 +1,6 @@
 package com.example.phmsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class VitalSignsActivity extends AppCompatActivity {
