8ef2fb65f5195020585efcafc227304d706ed758
Joshua Catalan <joshcatalan14@gmail.com>
2023-04-25 02:45:44 -0500

    History, Body Temp, and Date

--- PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
+++ PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
@@ -9,13 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class VitalSignsActivity extends AppCompatActivity {
 
-    private EditText editTextBloodPressure, editTextHeartRate, editTextOxygenSaturation;
-    private Button buttonSave;
+    private EditText editTextBloodPressure, editTextHeartRate, editTextOxygenSaturation, editTextBodyTemperature, editTextDate;
+    private Button buttonSave, buttonHistory;
     private TextView textViewSavedValues;
     private SharedPreferences sharedPreferences;
+    private ArrayList<String> history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,10 @@ public class VitalSignsActivity extends AppCompatActivity {
         editTextBloodPressure = findViewById(R.id.editTextBloodPressure);
         editTextHeartRate = findViewById(R.id.editTextHeartRate);
         editTextOxygenSaturation = findViewById(R.id.editTextOxygenSaturation);
+        editTextBodyTemperature = findViewById(R.id.editTextBodyTemperature);
+        editTextDate = findViewById(R.id.editTextDate);
         buttonSave = findViewById(R.id.buttonSave);
+        buttonHistory = findViewById(R.id.buttonHistory);
         textViewSavedValues = findViewById(R.id.textViewSavedValues);
         sharedPreferences = getSharedPreferences("vital_signs", MODE_PRIVATE);
 
@@ -33,9 +42,19 @@ public class VitalSignsActivity extends AppCompatActivity {
         String savedBloodPressure = sharedPreferences.getString("blood_pressure", "");
         String savedHeartRate = sharedPreferences.getString("heart_rate", "");
         String savedOxygenSaturation = sharedPreferences.getString("oxygen_saturation", "");
+        String savedBodyTemperature = sharedPreferences.getString("body_temperature", "");
         if (!savedBloodPressure.isEmpty() && !savedHeartRate.isEmpty() && !savedOxygenSaturation.isEmpty()) {
             textViewSavedValues.setText("Last saved vital signs: Blood pressure = " + savedBloodPressure +
-                    ", Heart rate = " + savedHeartRate + ", Oxygen saturation = " + savedOxygenSaturation);
+                    ", Heart rate = " + savedHeartRate + ", Oxygen saturation = " + savedOxygenSaturation + ", Body Temperature = " + savedBodyTemperature);
+        }
+
+        // Load history
+        history = new ArrayList<>();
+        for (int i = 1; i <= 15; i++) {
+            String historyItem = sharedPreferences.getString("history_" + i, "");
+            if (!historyItem.isEmpty()) {
+                history.add(historyItem);
+            }
         }
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
@@ -45,11 +64,13 @@ public class VitalSignsActivity extends AppCompatActivity {
                 String bloodPressure = editTextBloodPressure.getText().toString().trim();
                 String heartRate = editTextHeartRate.getText().toString().trim();
                 String oxygenSaturation = editTextOxygenSaturation.getText().toString().trim();
+                String BodyTemperature = editTextBodyTemperature.getText().toString().trim();
+                String TextDate = editTextDate.getText().toString().trim();
 
                 // Validate the entered values
-                if (bloodPressure.isEmpty() || heartRate.isEmpty() || oxygenSaturation.isEmpty()) {
+                if (bloodPressure.isEmpty() || heartRate.isEmpty() || oxygenSaturation.isEmpty() || BodyTemperature.isEmpty() || TextDate.isEmpty()) {
                     Toast.makeText(VitalSignsActivity.this, "Please enter all vital signs values", Toast.LENGTH_SHORT).show();
-                } else if (!isNumeric(bloodPressure) || !isNumeric(heartRate) || !isNumeric(oxygenSaturation)) {
+                } else if (!isNumeric(bloodPressure) || !isNumeric(heartRate) || !isNumeric(oxygenSaturation) || !isNumeric(BodyTemperature)) {
                     Toast.makeText(VitalSignsActivity.this, "Please enter valid vital signs values", Toast.LENGTH_SHORT).show();
                 } else {
                     // Save the entered values
@@ -57,19 +78,70 @@ public class VitalSignsActivity extends AppCompatActivity {
                     editor.putString("blood_pressure", bloodPressure);
                     editor.putString("heart_rate", heartRate);
                     editor.putString("oxygen_saturation", oxygenSaturation);
+                    editor.putString("body_temperature", BodyTemperature);
                     editor.apply();
-                    Toast.makeText(VitalSignsActivity.this, "Vital signs saved: Blood pressure = " + bloodPressure +
-                            ", Heart rate = " + heartRate + ", Oxygen saturation = " + oxygenSaturation, Toast.LENGTH_SHORT).show();
+
+                    // Get the saved values from history and add the new entry to history
+                    String savedValues = "Blood pressure = " + bloodPressure + ", Heart rate = " + heartRate + ", Oxygen saturation = " + oxygenSaturation + ", Body Temperature = " + BodyTemperature;
+                    history.add(0, savedValues);
+                    if (history.size() > 15) {
+                        history.remove(history.size() - 1);
+                    }
+                    String historyString = "";
+                    for (int i = 0; i < history.size(); i++) {
+                        historyString += history.get(i) + "\n";
+                    }
+                    editor.putString("history", historyString.trim());
+                    editor.apply();
+
+                    Toast.makeText(VitalSignsActivity.this, "Vital signs saved: " + savedValues, Toast.LENGTH_SHORT).show();
 
                     // Update saved values text view
-                    textViewSavedValues.setText("Last saved vital signs: Blood pressure = " + bloodPressure +
-                            ", Heart rate = " + heartRate + ", Oxygen saturation = " + oxygenSaturation);
+                    textViewSavedValues.setText("Last saved vital signs: " + savedValues);
+
+                    // Clear the fields
+                    editTextBloodPressure.getText().clear();
+                    editTextHeartRate.getText().clear();
+                    editTextOxygenSaturation.getText().clear();
+                    editTextBodyTemperature.getText().clear();
+                    editTextDate.getText().clear();
                 }
             }
         });
-    }
 
-    // Helper function to check if a string is numeric
+        buttonHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the saved history and sort it chronologically
+                String historyString = sharedPreferences.getString("history", "");
+                if (!historyString.isEmpty()) {
+                    String[] historyArray = historyString.split("\n");
+                    ArrayList<String> historyList = new ArrayList<>();
+                    Collections.addAll(historyList, historyArray);
+                    Collections.reverse(historyList);
+
+                    // Show the last 10 entries
+                    int numEntriesToShow = Math.min(10, historyList.size());
+                    String historyToShow = "";
+                    for (int i = 0; i < numEntriesToShow; i++) {
+                        historyToShow += historyList.get(i) + "\n";
+                    }
+
+                    // Create and show the dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(VitalSignsActivity.this);
+                    builder.setTitle("History");
+                    builder.setMessage("Last " + numEntriesToShow + " entries:\n" + historyToShow);
+                    builder.setPositiveButton("OK", null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    Toast.makeText(VitalSignsActivity.this, "No history yet", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
     private boolean isNumeric(String str) {
         try {
             double d = Double.parseDouble(str);
