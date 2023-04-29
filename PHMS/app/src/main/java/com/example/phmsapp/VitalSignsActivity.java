5a2aa7c1b23c2084eecb306289b7961e21b57a9c
Joshua Catalan <joshcatalan14@gmail.com>
2023-04-29 03:21:42 -0500

    Vital Signs: Calendar for date

--- PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
+++ PHMS/app/src/main/java/com/example/phmsapp/VitalSignsActivity.java
@@ -2,21 +2,22 @@ package com.example.phmsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class VitalSignsActivity extends AppCompatActivity {
 
@@ -25,6 +26,9 @@ public class VitalSignsActivity extends AppCompatActivity {
     private TextView textViewSavedValues;
     private SharedPreferences sharedPreferences;
     private ArrayList<String> history;
+
+    private String selectedDate = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +45,6 @@ public class VitalSignsActivity extends AppCompatActivity {
         textViewSavedValues = findViewById(R.id.textViewSavedValues);
         sharedPreferences = getSharedPreferences("vital_signs", MODE_PRIVATE);
 
-        // Load saved values
-        String savedBloodPressure = sharedPreferences.getString("blood_pressure", "");
-        String savedHeartRate = sharedPreferences.getString("heart_rate", "");
-        String savedOxygenSaturation = sharedPreferences.getString("oxygen_saturation", "");
-        String savedBodyTemperature = sharedPreferences.getString("body_temperature", "");
-        String savedDate = sharedPreferences.getString("date", "");
-        if (!savedBloodPressure.isEmpty() && !savedHeartRate.isEmpty() && !savedOxygenSaturation.isEmpty() && !savedDate.isEmpty()) {
-            textViewSavedValues.setText("Last saved vital signs: Blood pressure= " + savedBloodPressure +
-                    ", Heart rate= " + savedHeartRate + ", Oxygen saturation= " + savedOxygenSaturation + ", Body Temperature= " + savedBodyTemperature + ", Date: " + savedDate);
-        }
-
         // Load history
         history = new ArrayList<>();
         for (int i = 1; i <= 15; i++) {
@@ -61,48 +54,57 @@ public class VitalSignsActivity extends AppCompatActivity {
             }
         }
 
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show the DatePickerDialog when the date EditText is clicked
+                showDatePickerDialog();
+            }
+        });
+
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Get the entered values from the EditText views
                 // Get the entered values from the EditText views
                 String bloodPressure = editTextBloodPressure.getText().toString().trim();
                 String heartRate = editTextHeartRate.getText().toString().trim();
                 String oxygenSaturation = editTextOxygenSaturation.getText().toString().trim();
                 String BodyTemperature = editTextBodyTemperature.getText().toString().trim();
-                String Date = editTextDate.getText().toString().trim();
+                String Date = selectedDate.trim();
+
 
                 // Validate the entered values
                 if (bloodPressure.isEmpty() || heartRate.isEmpty() || oxygenSaturation.isEmpty() || BodyTemperature.isEmpty() || Date.isEmpty()) {
                     Toast.makeText(VitalSignsActivity.this, "Please enter all vital signs values", Toast.LENGTH_SHORT).show();
-                } else if (!isNumeric(bloodPressure) || !isNumeric(heartRate) || !isNumeric(oxygenSaturation) || !isNumeric(BodyTemperature) || isDateValid(Date, "MM/DD/YYYY")) {
+                } else if (isNumeric(bloodPressure) || isNumeric(heartRate) || isNumeric(oxygenSaturation) || isNumeric(BodyTemperature) || isDateValid(Date, "MM/DD/YYYY")) {
                     Toast.makeText(VitalSignsActivity.this, "Please enter valid vital signs values", Toast.LENGTH_SHORT).show();
                 } else {
+                    // Save the entered values
                     // Save the entered values
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putString("blood_pressure", bloodPressure);
                     editor.putString("heart_rate", heartRate);
                     editor.putString("oxygen_saturation", oxygenSaturation);
                     editor.putString("body_temperature", BodyTemperature);
-                    editor.putString("Date", Date);
+                    editor.putString("date", Date);
                     editor.apply();
 
+
                     // Get the saved values from history and add the new entry to history
-                    String savedValues = "Blood pressure= " + bloodPressure + ", Heart rate= " + heartRate + ", Oxygen saturation= " + oxygenSaturation + ", Body Temperature= " + BodyTemperature + ", Date: " + Date;
+                    String savedValues = "\n-Blood pressure: " + bloodPressure + " mmHg" + "\n-Heart rate: " + heartRate + " bpm" + "\n-Oxygen saturation: " + oxygenSaturation + " %" + "\n-Body Temperature: " + BodyTemperature + " °F" + "\n Date: " + Date;
                     history.add(0, savedValues);
                     if (history.size() > 15) {
                         history.remove(history.size() - 1);
                     }
-                    String historyString = "";
+                    StringBuilder historyString = new StringBuilder();
                     for (int i = 0; i < history.size(); i++) {
-                        historyString += history.get(i) + "\n";
+                        historyString.append(history.get(i)).append("\n");
                     }
-                    editor.putString("history", historyString.trim());
+                    editor.putString("history", historyString.toString().trim());
                     editor.apply();
 
                     Toast.makeText(VitalSignsActivity.this, "Vital signs saved: " + savedValues, Toast.LENGTH_SHORT).show();
-
-                    // Update saved values text view
-                    textViewSavedValues.setText("Last saved vital signs: " + savedValues);
 
                     // Clear the fields
                     editTextBloodPressure.getText().clear();
@@ -120,22 +122,17 @@ public class VitalSignsActivity extends AppCompatActivity {
                 // Get the saved history and sort it chronologically
                 String historyString = sharedPreferences.getString("history", "");
                 if (!historyString.isEmpty()) {
-                    String[] historyArray = historyString.split("\n");
-                    ArrayList<String> historyList = new ArrayList<>();
-                    Collections.addAll(historyList, historyArray);
-                    Collections.reverse(historyList);
-
                     // Show the last 10 entries
-                    int numEntriesToShow = Math.min(10, historyList.size());
-                    String historyToShow = "";
+                    int numEntriesToShow = Math.min(10, history.size());
+                    StringBuilder historyToShow = new StringBuilder();
                     for (int i = 0; i < numEntriesToShow; i++) {
-                        historyToShow += historyList.get(i) + "\n" + "\n";
+                        historyToShow.append(history.get(i)).append("\n");
                     }
 
                     // Create and show the dialog
                     AlertDialog.Builder builder = new AlertDialog.Builder(VitalSignsActivity.this);
                     builder.setTitle("History");
-                    builder.setMessage("Last " + numEntriesToShow + " entries:\n\n" + historyToShow);
+                    builder.setMessage("Last " + numEntriesToShow + " entries:\n" + historyToShow);
                     builder.setPositiveButton("OK", null);
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -147,13 +144,39 @@ public class VitalSignsActivity extends AppCompatActivity {
 
 
     }
+
+    private void showDatePickerDialog() {
+        // Get the current date as a default date
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a DatePickerDialog with the current date as a default date
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                VitalSignsActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // Store the selected date in the selectedDate variable
+                        selectedDate = String.format("%02d/%02d/%04d", monthOfYear + 1, dayOfMonth, year);
+                        editTextDate.setText(selectedDate);
+                    }
+                },
+                year, month, day
+        );
+
+        // Show the DatePickerDialog
+        datePickerDialog.show();
+    }
+
     private boolean isNumeric(String str) {
         try {
             double d = Double.parseDouble(str);
         } catch (NumberFormatException nfe) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
     public static boolean isDateValid(String dateStr, String format) {
